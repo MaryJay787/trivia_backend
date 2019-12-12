f65b161e4806c1dfa1aa0b5e1f2a0c8df69c33bf
MaryJay787 <josephmaryam.mj@gmail.com>
2019-12-11 20:22:04 -0500

    model controllers created

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,10 @@
 class CategoriesController < ApplicationController
+    def index
+        @categories = Category.all
+        json_response(@categories)
+    end
+
+    def show
+        json_response(@categories)
+    end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,10 @@
 class QuestionsController < ApplicationController
+    def index
+        @questions = Question.all
+        json_response(@questions)
+    end
+
+    def show
+        json_response(@questions)
+    end
 end

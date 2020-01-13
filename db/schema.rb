249e4073db36df2a4a9e410ffaefcad298d34e74
MaryJay787 <josephmaryam.mj@gmail.com>
2020-01-12 21:32:19 -0500

    removed foreign key from questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_001735) do
+ActiveRecord::Schema.define(version: 2020_01_13_022601) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -28,5 +28,4 @@ ActiveRecord::Schema.define(version: 2019_12_12_001735) do
     t.index ["category_id"], name: "index_questions_on_category_id"
   end
 
-  add_foreign_key "questions", "categories"
 end

6fc12d1c952431297f968309f5c93215f0b84110
MaryJay787 <josephmaryam.mj@gmail.com>
2020-01-12 21:42:09 -0500

    added cat key to questions table

---
+++ db/migrate/20200113024007_add_category_key_to_questions.rb
@@ -0,0 +1,5 @@
+class AddCategoryKeyToQuestions < ActiveRecord::Migration[6.0]
+  def change
+    add_column :questions, :category_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_023336) do
+ActiveRecord::Schema.define(version: 2020_01_13_024007) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_01_13_023336) do
     t.boolean "asked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
